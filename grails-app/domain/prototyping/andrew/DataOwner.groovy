2781b9710a754ce61841c0cc24785b3d87c8ceda
andrewbeatty <andrew.beatty@icloud.com>
2015-06-29 10:25:01 +0100

    prototype directory incomplete

---
+++ grails-app/domain/prototyping/andrew/DataOwner.groovy
@@ -0,0 +1,9 @@
+package prototyping.andrew
+
+/**
+ * Created by andrewbeatty on 29/06/15.
+ */
+interface DataOwner {
+    public addData(ProtoCriteriaData data)
+
+}
---
+++ grails-app/domain/prototyping/andrew/ProtoCriteriaData.groovy
@@ -0,0 +1,9 @@
+package prototyping.andrew
+
+class ProtoCriteriaData {
+
+    static belongsTo = [dataOwner:DataOwner]
+    String data
+    static constraints = {
+    }
+}
