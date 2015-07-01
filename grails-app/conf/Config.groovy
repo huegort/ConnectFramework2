27daf733fc23fa2cd1a4060cb1e5b7dea60b975e
andrewbeatty <andrew.beatty@icloud.com>
2015-07-01 14:32:27 +0100

    logging on

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -114,4 +114,8 @@ log4j.main = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+
+    debug  'com.guru.connectframework'  //  all the connect framework project
+
+
 }
