35b32517e99073391743322265ad12fd5310ec58
lmanuelcb <luis.manuel.cb@hotmail.com>
2015-07-02 10:56:11 +0100

    Takes in a parameter 'q' and gives response as JSON

--- grails-app/controllers/com/guru/connectframework/CfuserController.groovy
+++ grails-app/controllers/com/guru/connectframework/CfuserController.groovy
@@ -1,9 +1,20 @@
 package com.guru.connectframework
 
+import com.guru.connectframework.institution.Institution
+import grails.converters.JSON
+
 class CfuserController {
 
     def home() {
 
+    }
+
+    def institutionsList = {
+        def q = params['q']
+
+        def institutionList = Institution.findAllByNameLike("%${q}%")
+
+        render institutionList as JSON
     }
 
 }
