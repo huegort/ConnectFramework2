bedf6526f8dbf9e70f4ee6a097e033b9cba270fc
andrewbeatty <andrew.beatty@icloud.com>
2015-07-09 14:18:36 +0100

    criteria status display and name

--- src/groovy/com/guru/connectframework/Criteria/CriteriaStatus.groovy
+++ src/groovy/com/guru/connectframework/Criteria/CriteriaStatus.groovy
@@ -4,8 +4,26 @@
 package com.guru.connectframework.Criteria
 
 public enum CriteriaStatus{
-    PENDING,
-    APPROVED,
-    DECLINED,
-    INCOMPLETE,
+    PENDING(1,"Pending"),
+    APPROVED(2,"Approved"),
+    DECLINED(3,"Declined"),
+    INCOMPLETE(4,"Incomplete"),
+
+    int id
+    String display
+
+    public CriteriaStatus(int id, String display){
+        this.id = id
+        this.display = display
+    }
+    public int getId(){
+        return id
+    }
+    public String getName(){
+        return display
+    }
+    public String getDisplay(){
+        return display
+    }
+
 }
