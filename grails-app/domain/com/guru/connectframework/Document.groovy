64ee36825abe1660e2cd695dd58f398999036793
andrewbeatty <andrew.beatty@icloud.com>
2015-07-13 16:40:34 +0100

    updated the Document domain class to include filetype and size

--- grails-app/domain/com/guru/connectframework/Document.groovy
+++ grails-app/domain/com/guru/connectframework/Document.groovy
@@ -1,12 +1,19 @@
 package com.guru.connectframework
 
+import com.guru.connectframework.documents.FileType
+
 class Document {
     String filename
     byte[] filedata
     Date uploadDate = new Date()
+    FileType fileType
+    long fileSize
+
+    Date dateArchived
 
     static constraints = {
         filename(blank:false,nullable:false)
         filedata(blank: true, nullable:true, maxSize:1073741824)
+        dateArchived nullable: true
     }
 }
---
+++ src/groovy/com/guru/connectframework/documents/FileType.groovy
@@ -0,0 +1,11 @@
+package com.guru.connectframework.documents
+
+/**
+ * Created by andrewbeatty on 13/07/15.
+ */
+enum FileType {
+    PDF,
+    DOCX,
+    XLSX
+
+}
