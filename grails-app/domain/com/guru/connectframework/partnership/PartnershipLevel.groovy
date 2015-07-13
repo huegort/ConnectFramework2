bc92569ed1aa1cd730e1b371ee4de641a7c078c9
lmanuelcb <luis.manuel.cb@hotmail.com>
2015-07-13 16:13:30 +0100

    PartnershipLevel added dateArchived

--- grails-app/domain/com/guru/connectframework/partnership/PartnershipLevel.groovy
+++ grails-app/domain/com/guru/connectframework/partnership/PartnershipLevel.groovy
@@ -1,6 +1,5 @@
 package com.guru.connectframework.partnership
 
-import com.guru.connectframework.Role
 import com.guru.connectframework.User
 import com.guru.connectframework.criteria.CriteriaContainer
 
@@ -10,12 +9,13 @@ class PartnershipLevel {
     String urlToDoc
     CriteriaContainer criteriaContainer
     int level
-    int defaultDurationInYears = 5
+    int durationOfApprovalInYears = 5
+    Date dateArchived
 
     static constraints = {
         description nullable: true
         urlToDoc nullable:  true
-
+        dateArchived nullable: true
     }
     String toString() {
         return name
