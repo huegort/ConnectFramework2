28e7d0789a3dbdb720aa5a36cf1bae50c8dfbc3c
andrewbeatty <andrew.beatty@icloud.com>
2015-08-04 16:20:07 +0100

    taken out inheritance in domian classes it is acting strange

--- src/groovy/com/guru/connectframework/ApprovableTypeEnity.groovy
+++ src/groovy/com/guru/connectframework/ApprovableTypeEnity.groovy
@@ -4,6 +4,8 @@ package com.guru.connectframework
  * Created by andrewbeatty on 22/07/15.
  */
 abstract class ApprovableTypeEnity extends CfEntity{
+    // not currently used
+
     Role creatorRole
     Role endorserRole
     Role approverRole
--- src/groovy/com/guru/connectframework/CfEntity.groovy
+++ src/groovy/com/guru/connectframework/CfEntity.groovy
@@ -4,7 +4,7 @@ package com.guru.connectframework
  * Created by andrewbeatty on 22/07/15.
  */
 abstract class CfEntity {
-
+    // not currently used
 
     def toStringForAudit(){
        //TODO decide default action for this (JSON??)
